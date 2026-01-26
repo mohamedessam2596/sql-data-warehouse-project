751ff8e313c0e5355363c263f8bbb4f5944b8c6b
mohamedessam2596 <mohamedessam2596@gmail.com>
2026-01-26 12:54:47 +0200

    Update transformation.sql

--- scripts/silver/transformation.sql
+++ scripts/silver/transformation.sql
@@ -4,7 +4,7 @@ select
 prd_id,
 substring(prd_key,7,LEN(prd_key)) as prd_key ,
 replace(substring(prd_key,1,5),'-','_') as cat_ID,
-isnull(prd_cost,0),
+isnull(prd_cost,0) as prd_cost,
 case upper(Trim(prd_line)) 
 	when 'M' Then 'Mountain'
 	when 'R' Then 'Road'
@@ -21,8 +21,8 @@ case upper(Trim(prd_line))
             ORDER BY prd_start_dt
         )
     ) AS prd_end_dt
-    into silver.[crm_prd_info]
-	from [bronze].[crm_prd_info]
+    into  silver.crm_prd_info
+	from [bronze].[crm_prd_info]]
 
 
 
