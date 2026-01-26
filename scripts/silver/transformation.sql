be289bee7ccaf74021fd21ef50d73e76d281eae7
mohamedessam2596 <mohamedessam2596@gmail.com>
2026-01-26 12:50:05 +0200

    Update transformation.sql

--- scripts/silver/transformation.sql
+++ scripts/silver/transformation.sql
@@ -21,5 +21,47 @@ case upper(Trim(prd_line))
             ORDER BY prd_start_dt
         )
     ) AS prd_end_dt
-
+    into into silver.[crm_prd_info]
 	from [bronze].[crm_prd_info]
+
+
+
+/*transformation and load for silver.[crm_cust_info] */
+
+with max_date as (
+select *,
+	rank() over(partition by cst_id order by cst_create_date desc) as rank_max_date 
+from bronze.[crm_cust_info])
+select cst_id,cst_key,
+		trim(COALESCE(cst_firstname+' '+cst_lastname,cst_firstname,cst_lastname)) as customer_name,
+		case when upper(trim(cst_marital_status))='M' then 'married'
+		 when upper(trim(cst_marital_status))='S' then 'single' 
+		 else 'n/a' end as cst_marital_status,
+	case when upper(trim(cst_gndr))='M' then 'male'
+		 when upper(trim(cst_gndr))='F' then 'female' 
+		 else 'n/a' end as cst_gndr,
+		 cst_create_date
+		 into silver.[crm_cust_info]
+from max_date
+where rank_max_date=1 and cst_id is not null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
